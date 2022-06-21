388d1f5c96fab43b142ca9f52cb5e1655c4828cd
Asus <ivanova.yevheniia@gmail.com>
2022-06-21 14:38:54 +0300

    it works!

--- viginere.py
+++ viginere.py
@@ -1,3 +1,8 @@
+
+# LOWERCASE ENCRYPTION IMPLEMENTATION ONLY
+
+OFFSET_LOWERCASE = 97
+
 def keygenerate(plaintext, key):
   key = list(key)
   if len(plaintext) == len(key):
@@ -12,24 +17,23 @@ def keygenerate(plaintext, key):
 def encryption(plaintext, key):
   cipher = []
   for i in range(len(plaintext)):
-    x = (ord(plaintext[i]) + ord(key[i])) % 26
-    x += ord('A')
-    cipher.append(chr(x))
+    x = (ord(plaintext[i]) + ord(key[i]) - 2*OFFSET_LOWERCASE) % 26
+    cipher.append(chr(x + OFFSET_LOWERCASE))
   return("" .join(cipher))
 
 
 def decryption(cipher, key):
   plaintext = []
   for i in range(len(cipher)):
-    x = (ord(cipher[i]) - ord(key[i]) + 26) % 26
-    x += ord('A')
-    plaintext.append(chr(x))
+    x = (ord(cipher[i]) - ord(key[i])) % 26
+    plaintext.append(chr(x + OFFSET_LOWERCASE))
   return("" .join(plaintext))
 
 
 if __name__ == "__main__":
   plaintext = input("Enter the message: ")
-  key = input("Enter the key: ")
+  keyword = input("Enter the keyword: ")
+  key = keygenerate(plaintext, keyword)
   cipher = encryption(plaintext, key)
   print("Encrypted message: ", cipher)
   print("Decrypted message: ", decryption(cipher, key))
