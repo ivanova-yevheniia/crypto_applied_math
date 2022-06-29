d1a18b0e1641dbc300720ab1722606868281d392
Asus <ivanova.yevheniia@gmail.com>
2022-06-29 19:19:50 +0300

    completed

--- display_key.py
+++ display_key.py
@@ -1,11 +1,11 @@
 import rsa
 
-with open('private_key_1024.pem', 'rb') as inf:
+with open('private_key_16.pem', 'rb') as inf:
     priv_key = rsa.PrivateKey.load_pkcs1(inf.read())
 
 print("n={} d={}".format(priv_key.n, priv_key.d))
 
-with open('public_key_1024.pem', 'rb') as inf:
+with open('public_key_16.pem', 'rb') as inf:
     public_key = rsa.PublicKey.load_pkcs1(inf.read())
 
 print("n={} e={}".format(public_key.n, public_key.e))
--- genkeys.py
+++ genkeys.py
@@ -1,6 +1,6 @@
 import rsa
 
-KEY_SIZE = 1024
+KEY_SIZE = 16
 (pub_key, priv_key) = rsa.newkeys(KEY_SIZE)
 
 with open('private_key_{}.pem'.format(KEY_SIZE), 'wb') as outf:
